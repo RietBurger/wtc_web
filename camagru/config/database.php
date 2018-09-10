cbf645c2d899c9c161192df14e6ef64db4199ffe
Jeanette Henriette BURGER <rburger@e4r20p1.wethinkcode.co.za>
2018-09-10 14:13:06 +0200

    modified

--- camagru/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$DB_DSN = "mysql:host=localhost:3306";
-$DB_USER = "root";
-$DB_PASSWORD = "Grammy21";
-
-?>
--- matcha/config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$DB_USER = "root";
-$DB_PASSWD = "Grammy21";
-$DB_DSN = "mysql:host=localhost:3306";
-
-?>
